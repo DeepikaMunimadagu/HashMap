d3c28f3c441e03ada226f1f7760d132be99681f4
Deepika Chandrashekar Munimadagu <121370042+DeepikaMunimadagu@users.noreply.github.com>
2023-07-12 10:54:08 +0530

    Create hm5.java

---
+++ hm5.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Classroom {
+    public static void main(String args[]){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(2);
+        set.add(1);
+        System.out.println(set);
+
+        if(set.contains(2)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+        set.remove(2);
+        if(set.contains(2)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        System.out.println(set);
+        System.out.println(set.size());
+        set.clear();
+        System.out.println(set.size());
+        System.out.println(set.isEmpty());
+    }
+}
